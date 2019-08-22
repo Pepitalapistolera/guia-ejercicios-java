afd0c785653405a2e5e535a750318149a9c597e5
Pepitalapistolera <acrodri991@gmail.com>
2019-08-22 16:59:04 -0300

    ejercicio1

--- src/main/java/ar.edu.unahur.obj2.ejercicio1/Ejercicio1.java
+++ src/main/java/ar.edu.unahur.obj2.ejercicio1/Ejercicio1.java
@@ -1,8 +1,19 @@
 package ar.edu.unahur.obj2.ejercicio1;
+import java.util.Scanner;
 
 public class Ejercicio1 {
 
-    public int[] ordenar() {
-        return new int[] {};
+    public static void main (String[]args) {
+    int numeros[]  = new int [10];
+    Scanner num = new Scanner(System.in);
+    System.out.println("Ingrese 10 numeros: ");
+    for(int i=0; i<numeros.length; i++) {
+        numeros[i] = num.nextInt();
+    }
+
+    System.out.println("En orden inverso: ");
+    for(int i=9; i>=0; i--){
+        System.out.println(numeros[i]);
+    }
     }
 }
